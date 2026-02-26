fc16d6153e5387947e3f6b169e4f10f649928229
parikshith832 <parikshith832@gmail.com>
2026-02-26 13:17:20 +0530

    Day4

--- Day4/validstring.java
+++ Day4/validstring.java
@@ -1,36 +1,23 @@
 package Day4;
 
 public class validstring {
-    public boolean isPalindrome(String s) {
+    String s;
+
+    public boolean isPalindrome1(String s) {
+        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
         int left = 0;
         int right = s.length() - 1;
-
         while (left < right) {
-
-            // Skip non-alphanumeric characters
-            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
-                left++;
-            }
-
-            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
-                right--;
-            }
-
-            // Compare lowercase characters
-            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+            if (s.charAt(left++) != s.charAt(right--)) {
                 return false;
             }
-
-            left++;
-            right--;
         }
-
         return true;
     }
 
     public static void main(String[] args) {
         validstring ob = new validstring();
-        if (ob.isPalindrome("JavaJ")) {
+        if (ob.isPalindrome1("JavaJ")) {
             System.out.println("palindrome");
 
         } else {
