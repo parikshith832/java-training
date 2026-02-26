6ddded70f1b97af36622fea79b3a57cdd73564f2
parikshith832 <parikshith832@gmail.com>
2026-02-26 12:42:04 +0530

    day4

--- Day4/operation.java
+++ Day4/operation.java
@@ -17,10 +17,30 @@ public class operation {
             System.out.println("substring: " + str.substring(0, 3));
         }
         String str2 = "Java";
-        System.out.println("Concat " + str.concat(str2));
-        System.out.println("using + " + str + str2);
+        System.out.println("Concat: " + str.concat(str2));
+        System.out.println("using +: " + str + str2);
         System.out.println("equals: " + str.equals(str2));
         System.out.println("equals ignore case: " + str.equalsIgnoreCase(str2));
         System.out.println("Replace: " + str2.replace("Java", "C++"));
+        System.out.println("Trimmed: " + str.trim());
+        System.out.println("Starts with: " + str.startsWith("J"));
+        System.out.println("endsS with: " + str.endsWith("a"));
+        System.out.println("Index of J:" + str.indexOf("J"));
+        String words[] = str.split(" ");
+        System.out.println("Split by Space:");
+        for (String i : words) {
+            System.out.println((i));
+        }
+        System.out.println("checking each one of the character: ");
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (Character.isLetter(ch)) {
+                System.out.println(ch + " is a character");
+            } else if (Character.isDigit(ch)) {
+                System.out.println(ch + " is a digit");
+            } else {
+                System.out.println(ch + " is a special character");
+            }
+        }
     }
 }
