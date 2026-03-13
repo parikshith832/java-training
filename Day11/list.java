90939ea3c125e73844fe9cfe2c180f8800fd0939
Parikshith S <parikshith832@gmail.com>
2026-03-13 12:01:05 +0530

    Day11

--- Day11/list.java
+++ Day11/list.java
@@ -43,6 +43,34 @@ public class list {
         head = newnode;
     }
 
+    void reverse() {
+        Node cur = head;
+        Node prev = null;
+        Node next;
+        while (cur != null) {
+            next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        head = prev;
+    }
+
+    void delete_at_first() {
+        Node temp = head;
+        if (head == null) {
+            System.out.println("no elements present.");
+            return;
+        }
+        if (temp.next == null) {
+            head = null;
+            return;
+        }
+
+        head = head.next;
+        temp = null;
+    }
+
     void delete_at_end() {
         Node temp = head;
         if (temp == null) {
@@ -72,22 +100,21 @@ public class list {
     }
 
     public static void main(String[] args) {
+        System.out.println("Linked List: ");
         list ob = new list();
         ob.insert_at_end(10);
         ob.insert_at_end(20);
         ob.insert_at_end(30);
         ob.insert_at_end(40);
         ob.insert_at_first(50);
+        ob.display();
+        ob.delete_at_first();
+        ob.display();
         ob.insert_at_first(60);
         ob.insert_at_end(70);
         ob.delete_at_end();
-        ob.delete_at_end();
-        ob.delete_at_end();
-        ob.delete_at_end();
-        ob.delete_at_end();
-        ob.delete_at_end();
-
-        System.out.println("Linked List: ");
+        ob.display();
+        ob.reverse();
         ob.display();
 
     }
