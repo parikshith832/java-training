2af7c7827fba457889e89557abf1d3b81a00c3ca
Parikshith S <parikshith832@gmail.com>
2026-06-27 09:18:56 +0530

    26-06

---
+++ Day16/Engine.java
@@ -0,0 +1,37 @@
+package Day16;
+
+/**
+ * spring1
+ */
+public class Engine {
+
+    public void start() {
+        System.out.println("Engine Started");
+
+    }
+}
+
+class car {
+    private Engine engine = new Engine();
+
+    public void drive() {
+        engine.start();
+        System.out.println("Car is running");
+    }
+}
+
+/**
+ * InnerEngine
+ */
+public class InnerEngine {
+    public static void main(String[] args) {
+
+        Engine engine = new Engine();
+        car = new car(engine);
+        /*
+         * spring-> spring creates object and it will manage lifecycle
+         * IOC-> inversion of control
+         * control of object creation moves from program to spring
+         */
+    }
+}
