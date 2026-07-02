bd6a71d5f70e0238a11ee2fcc99af8fc7aded918
Parikshith S <parikshith832@gmail.com>
2026-07-02 12:57:07 +0530

    Day19

---
+++ Day19/hello.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float grade = 0.05;
+    string name = "Darshan";
+    bool flag = 1;
+    string sal = "35,000";
+    cout << grade;
+    cout << "\n";
+    cout << name;
+    cout << "\n";
+    cout << flag;
+    cout << "\n";
+    cout << sal;
+    cout << "\n";
+    cout << "Hello world\n";
+    cout << "Parikshith S";
+    cout << "\n";
+    int a = 1;
+    int b = 2;
+    int res = a + b;
+    cout << res;
+
+    return 0;
+}
